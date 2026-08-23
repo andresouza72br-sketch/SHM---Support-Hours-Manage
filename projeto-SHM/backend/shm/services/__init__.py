46659b13e20e9e88f31a84f3bd255fdb6c1f66fd
André Souza <andresouza72br@gmail.com>
2026-08-23 13:17:45 -0300

    chore: remove subpasta legada projeto-SHM para inicializacao da arquitetura limpa na raiz

--- projeto-SHM/backend/shm/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from .contract_service import ContractService
-from .timeline_service import TimelineService
-from .workflow_service import WorkflowService
-
-__all__ = [
-    "TimelineService",
-    "ContractService",
-    "WorkflowService",
-]
--- projeto-SHM/backend/shm/tests/__init__.py
+++
@@ -1,1 +0,0 @@
-"""SHM test suite package."""
